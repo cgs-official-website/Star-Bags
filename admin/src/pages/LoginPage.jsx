a0932d064a495fc8f68c5a44256c4e8784d00d40
sanjai-web <ksvsanjai20@gmail.com>
2026-05-12 13:57:50 +0530

    Admin Page Setups

---
+++ admin/src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import '../style.css'
+
+function LoginPage() {
+  return (
+    <div className='login' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw'}}>
+
+
+    <div>
+      <h1>Login</h1>
+    </div>
+
+    </div>
+  )
+}
+
+export default LoginPage
