84604adb36bef196b16f4941f3455af1558b8201
sanjai-web <ksvsanjai20@gmail.com>
2026-05-14 18:07:40 +0530

    App.js update

--- leather-app/src/App.jsx
+++ leather-app/src/App.jsx
@@ -16,13 +16,13 @@ import AdminLogin from './pages/Admin/AdminLogin';
 import AdminSignup from './pages/Admin/AdminSignup';
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import Coupons from './pages/Admin/Coupons';
-import ForgetPassword from './pages/Admin/ForgetPassword';
+import AdminForgetPassword from './pages/Admin/ForgetPassword';
 import OrderManagement from './pages/Admin/OrderManagement';
 import PaymentDetails from './pages/Admin/PaymentDetails';
 import ProductManagement from './pages/Admin/ProductManagement';
-import Settings from './pages/Admin/Settings';
+import AdminSettings from './pages/Admin/Settings';
 import ReportAnalysis from './pages/Admin/ReportAnalysis';
-import ResetPassword from './pages/Admin/ResetPassword';
+import AdminResetPassword from './pages/Admin/ResetPassword';
 import StoreDetails from './pages/Admin/StoreDetails';
 
 const App = () => {
@@ -46,14 +46,14 @@ const App = () => {
         {/* Admin Routes */}
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin/signup" element={<AdminSignup />} />
-        <Route path="/admin/forget-password" element={<ForgetPassword />} />
-        <Route path="/admin/reset-password" element={<ResetPassword />} />
+        <Route path="/admin/forget-password" element={<AdminForgetPassword />} />
+        <Route path="/admin/reset-password" element={<AdminResetPassword />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/coupons" element={<Coupons />} />
         <Route path="/admin/order-management" element={<OrderManagement />} />
         <Route path="/admin/payment-details" element={<PaymentDetails />} />
         <Route path="/admin/product-management" element={<ProductManagement />} />
-        <Route path="/admin/settings" element={<Settings />} />
+        <Route path="/admin/settings" element={<AdminSettings />} />
         <Route path="/admin/report-analysis" element={<ReportAnalysis />} />
         <Route path="/admin/store-details" element={<StoreDetails />} />
       </Routes>
